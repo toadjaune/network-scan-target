4c7c6b8edb398553451671305d529b7a974ca85b
Arnaud Venturi <git@toadjaune.eu>
2024-09-10 15:10:17 +0200

    Add udp sockets

--- listen.py
+++ listen.py
@@ -7,12 +7,29 @@ import errno
 logger = logging.getLogger()
 
 
-class EchoServerProtocol(asyncio.Protocol):
+# This code is heavily inspired by the examples at https://docs.python.org/3/library/asyncio-protocol.html
+
+
+class TCPServerProtocol(asyncio.Protocol):
     def connection_made(self, transport):
+        """Gets called whenever a client connects (TCP handshake)"""
         peername = transport.get_extra_info("peername")
         print("Connection from {}".format(peername))
         transport.write(b"Connection attempt OK\n")
         transport.close()
+
+
+class UDPServerProtocol:
+    def connection_made(self, transport):
+        """Gets called when the socket is opened"""
+        self.transport = transport
+
+    def datagram_received(self, data, addr):
+        """Gets called whenever a new datagram is received on the socket"""
+        message = data.decode()
+        print("Received %r from %s" % (message, addr))
+        print("Send %r to %s" % (message, addr))
+        self.transport.sendto(data, addr)
 
 
 def parse_args():
@@ -34,16 +51,33 @@ def parse_args():
     return parser.parse_args()
 
 
-async def start_server_on_port(port: int):
+async def open_tcp_socket_on_port(port: int):
     # Get a reference to the event loop as we plan to use low-level APIs.
     loop = asyncio.get_running_loop()
     try:
         server = await loop.create_server(
-            lambda: EchoServerProtocol(),
+            lambda: TCPServerProtocol(),
             "",  # listen on all available interfaces
             port,
         )
         await server.serve_forever()
+    except OSError as e:
+        if e.errno == errno.EADDRINUSE:
+            logger.warning(f"Could not listen on port {port}, already in use")
+        else:
+            raise e
+
+
+async def open_udp_socket_on_port(port: int):
+    # Get a reference to the event loop as we plan to use low-level APIs.
+    loop = asyncio.get_running_loop()
+    try:
+        transport, protocol = await loop.create_datagram_endpoint(
+            # TODO : address family
+            lambda: UDPServerProtocol(),
+            local_addr=("127.0.0.1", port),
+        )
+        await asyncio.sleep(3600)
     except OSError as e:
         if e.errno == errno.EADDRINUSE:
             logger.warning(f"Could not listen on port {port}, already in use")
@@ -57,7 +91,8 @@ async def main():
     coroutines = []
 
     for port in range(args.port_min, args.port_max):
-        coroutines.append(start_server_on_port(port))
+        coroutines.append(open_tcp_socket_on_port(port))
+        coroutines.append(open_udp_socket_on_port(port))
 
     await asyncio.gather(*coroutines)
 
