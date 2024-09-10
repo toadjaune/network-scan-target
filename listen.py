f5e384be95d0243d1297792f032359d50338eacb
Arnaud Venturi <git@toadjaune.eu>
2024-09-10 18:18:46 +0200

    Proper logging

--- listen.py
+++ listen.py
@@ -6,9 +6,6 @@ import logging
 import errno
 
 
-logger = logging.getLogger()
-
-
 # This code is heavily inspired by the examples at https://docs.python.org/3/library/asyncio-protocol.html
 
 
@@ -16,7 +13,7 @@ class TCPServerProtocol(asyncio.Protocol):
     def connection_made(self, transport):
         """Gets called whenever a client connects (TCP handshake)"""
         peername = transport.get_extra_info("peername")
-        print("Connection from {}".format(peername))
+        logging.info("Connection from {}".format(peername))
         transport.write(b"Connection attempt OK\n")
         transport.close()
 
@@ -30,11 +27,19 @@ class UDPServerProtocol:
         """Gets called whenever a new datagram is received on the socket"""
         message = f"Received probe from {addr}"
         data = str.encode(message)
-        logger.info(message)
+        logging.info(message)
         self.transport.sendto(data, addr)
 
 
 def parse_args():
+    loglevels = {
+        "critical": logging.CRITICAL,
+        "error": logging.ERROR,
+        "warn": logging.WARNING,
+        "warning": logging.WARNING,
+        "info": logging.INFO,
+        "debug": logging.DEBUG,
+    }
     parser = argparse.ArgumentParser(
         description="Listen on many ports on TCP and UDP, to act as a scan target for black-box firewall testing"
     )
@@ -50,7 +55,16 @@ def parse_args():
         default=65535,
         help="Upper bound of the port range to listen on",
     )
-    return parser.parse_args()
+    parser.add_argument(
+        "--loglevel",
+        choices=loglevels.keys(),
+        action="store",
+        default="warning",
+        help="Provide logging level. default=warning",
+    )
+    parsed_arguments = parser.parse_args()
+    logging.basicConfig(level=loglevels[parsed_arguments.loglevel])
+    return parsed_arguments
 
 
 async def open_tcp_socket_on_port(port: int):
@@ -62,10 +76,11 @@ async def open_tcp_socket_on_port(port: int):
             "::",  # listen on all available interfaces, with both IPv4 and IPv6
             port,
         )
+        logging.debug(f"Opened TCP port {port}")
         await server.serve_forever()
     except OSError as e:
         if e.errno == errno.EADDRINUSE:
-            logger.warning(f"Could not listen on TCP port {port}, already in use")
+            logging.warning(f"Could not listen on TCP port {port}, already in use")
         else:
             raise e
 
@@ -78,20 +93,21 @@ async def open_udp_socket_on_port(port: int):
             lambda: UDPServerProtocol(),
             local_addr=("::", port),
         )
+        logging.debug(f"Opened UDP port {port}")
         await asyncio.sleep(3600)
     except OSError as e:
         if e.errno == errno.EADDRINUSE:
-            logger.warning(f"Could not listen on UDP port {port}, already in use")
+            logging.warning(f"Could not listen on UDP port {port}, already in use")
         else:
             raise e
 
 
 async def main():
-    args = parse_args()
+    cli_args = parse_args()
 
     coroutines = []
 
-    for port in range(args.port_min, args.port_max + 1):
+    for port in range(cli_args.port_min, cli_args.port_max + 1):
         coroutines.append(open_tcp_socket_on_port(port))
         coroutines.append(open_udp_socket_on_port(port))
 
