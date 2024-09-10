9fa1cf027bec761de92aac4899cdbf62a6323e52
Arnaud Venturi <git@toadjaune.eu>
2024-09-10 11:47:37 +0200

    Directly answer and close connection without waiting for input

--- listen.py
+++ listen.py
@@ -6,17 +6,8 @@ class EchoServerProtocol(asyncio.Protocol):
     def connection_made(self, transport):
         peername = transport.get_extra_info('peername')
         print('Connection from {}'.format(peername))
-        self.transport = transport
-
-    def data_received(self, data):
-        message = data.decode()
-        print('Data received: {!r}'.format(message))
-
-        print('Send: {!r}'.format(message))
-        self.transport.write(data)
-
-        print('Close the client socket')
-        self.transport.close()
+        transport.write(b"Connection attempt OK\n")
+        transport.close()
 
 
 async def main():
