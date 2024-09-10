95fea665f55523288bc1525e3176d0103b12f8d1
Arnaud Venturi <git@toadjaune.eu>
2024-09-10 11:47:37 +0200

    Add argument parser

--- listen.py
+++ listen.py
@@ -1,4 +1,4 @@
-
+import argparse
 import asyncio
 
 
@@ -11,13 +11,20 @@ class EchoServerProtocol(asyncio.Protocol):
 
 
 async def main():
+
+    parser = argparse.ArgumentParser(description='Listen on many ports, to act as a scan target for black-box firewall testing')
+    parser.add_argument('--port-min', type=int, default=1,     help='Lower bound of the port range to listen on')
+    parser.add_argument('--port-max', type=int, default=65535, help='Upper bound of the port range to listen on')
+    args = parser.parse_args()
+
     # Get a reference to the event loop as we plan to use
     # low-level APIs.
     loop = asyncio.get_running_loop()
 
     server = await loop.create_server(
         lambda: EchoServerProtocol(),
-        '', 8888)
+        '', # listen on all available interfaces
+        8888)
 
     async with server:
         await server.serve_forever()
