a87f09a983503dce14bde5ec9a547ec40fe2d907
Arnaud Venturi <git@toadjaune.eu>
2024-09-10 14:20:57 +0200

    Small refacto

--- listen.py
+++ listen.py
@@ -9,42 +9,57 @@ logger = logging.getLogger()
 
 class EchoServerProtocol(asyncio.Protocol):
     def connection_made(self, transport):
-        peername = transport.get_extra_info('peername')
-        print('Connection from {}'.format(peername))
+        peername = transport.get_extra_info("peername")
+        print("Connection from {}".format(peername))
         transport.write(b"Connection attempt OK\n")
         transport.close()
 
+
 def parse_args():
-    parser = argparse.ArgumentParser(description='Listen on many ports, to act as a scan target for black-box firewall testing')
-    parser.add_argument('--port-min', type=int, default=1,     help='Lower bound of the port range to listen on')
-    parser.add_argument('--port-max', type=int, default=65535, help='Upper bound of the port range to listen on')
+    parser = argparse.ArgumentParser(
+        description="Listen on many ports, to act as a scan target for black-box firewall testing"
+    )
+    parser.add_argument(
+        "--port-min",
+        type=int,
+        default=1,
+        help="Lower bound of the port range to listen on",
+    )
+    parser.add_argument(
+        "--port-max",
+        type=int,
+        default=65535,
+        help="Upper bound of the port range to listen on",
+    )
     return parser.parse_args()
 
-async def main():
 
-    args = parse_args()
-
+async def start_server_on_port(port: int):
     # Get a reference to the event loop as we plan to use low-level APIs.
     loop = asyncio.get_running_loop()
+    try:
+        server = await loop.create_server(
+            lambda: EchoServerProtocol(),
+            "",  # listen on all available interfaces
+            port,
+        )
+        await server.serve_forever()
+    except OSError as e:
+        if e.errno == errno.EADDRINUSE:
+            logger.warning(f"Could not listen on port {port}, already in use")
+        else:
+            raise e
+
+
+async def main():
+    args = parse_args()
 
     coroutines = []
 
     for port in range(args.port_min, args.port_max):
-        try:
-            server = await loop.create_server(
-                lambda: EchoServerProtocol(),
-                '', # listen on all available interfaces
-                port)
-            coroutines.append(server.serve_forever())
-        except OSError as e:
-            if e.errno == errno.EADDRINUSE:
-                logger.warn(f"Could not listen on port {port}, already in use")
-            else:
-                raise e
+        coroutines.append(start_server_on_port(port))
 
-
-    async with server:
-        await asyncio.gather(*coroutines)
+    await asyncio.gather(*coroutines)
 
 
 asyncio.run(main())
